5ae5b903a72c744ba802d9c2b1520111a090eb74
Даниил Демьяненко <daniil-demyanenko1@mail.ru>
2019-12-12 21:27:13 +0300

    Добавьте файлы проекта.

---
+++ ScreenOff/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenOff
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        private static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        private static extern void mouse_event(IntPtr dwFlags, uint dx, uint dy, uint dwData, UIntPtr dwExtraInfo);
+
+        private const int MOVE = 0x0001;
+        private const int HWND_BROADCAST = 0xffff;
+        private const int WM_SYSCOMMAND = 0x0112;
+        private const int SC_MONITORPOWER = 0xF170;
+
+        static public void SomeMethod()
+        {
+            SendMessage((IntPtr)HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, 2);
+            mouse_event((IntPtr)MOVE, 0, 0, 0, UIntPtr.Zero);
+        }
+        static void Main(string[] args)
+        {
+            SomeMethod();
+        }
+    }
+}
